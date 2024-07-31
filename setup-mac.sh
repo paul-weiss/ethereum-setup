3023b173fd2e989c424af9a83a8d56cf1ec0f9c4
paul <paulweiss@protonmail.com>
2024-07-31 15:16:00 -0400

    Update setup-mac.sh

--- setup-mac.sh
+++ setup-mac.sh
@@ -2,6 +2,9 @@
 
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup update stable
+# note this is may need to be changed based on your chipset
+rustup default stable-aarch64-apple-darwin
 
 # install llvm
 brew install llvm pkg-config
